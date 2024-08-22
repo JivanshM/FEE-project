ce7ceafc011e885763c7c7687c4f68929e98809c
JivanshM <140148509+JivanshM@users.noreply.github.com>
2024-08-22 23:04:23 +0530

    Delete FEE-project-main directory

--- FEE-project-main/assests/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const burgerMenu = document.querySelector('.burger-menu');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  
-  burgerMenu.addEventListener('click', function () {
-      mobileMenu.classList.toggle('active');
-  });
-});
